c8beba2e64aa92fdd0c638fee589d809216beab1
Caldaga <caldega@gmail.com>
2018-07-19 22:46:49 -0500

    Add files via upload

--- cptrax/php/sqlconnect.php
+++ cptrax/php/sqlconnect.php
@@ -2,7 +2,7 @@
 
 	require 'sqlserver.php';
 	
-	$database = array("UID" => "Brandon@cptraxsql", "pwd" => "#13417Meat1001", "Database" => "Audit_Data", "ReturnDatesAsStrings" => true, "Encrypt" => 1, "TrustCertificate" => 0);
+	$database = array("UID" => "Brandon@cptraxsql", "pwd" => "#13417Meat1001", "Database" => "Audit_Data", "ReturnDatesAsStrings" => true, "Encrypt" => 1);
 	$conn = sqlsrv_connect($server, $database); 
 	if ($conn === false) die("<pre>".print_r(sqlsrv_errors(), true));
 
